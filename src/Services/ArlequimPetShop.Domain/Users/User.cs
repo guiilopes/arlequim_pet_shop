d0c0cd61077e9802f31cc529e2e3187642970697
guilherme.lopes <guilherme.lopes@opeacapital.com>
2025-06-25 12:58:25 -0300

    feat: implementa regra para senha forte

--- src/Services/ArlequimPetShop.Domain/Users/User.cs
+++ src/Services/ArlequimPetShop.Domain/Users/User.cs
@@ -1,18 +1,23 @@
 ﻿using ArlequimPetShop.SharedKernel;
 using ArlequimPetShop.SharedKernel.Enums;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.IdentityModel.Tokens;
 using SrShut.Common;
+using SrShut.Common.Exceptions;
 using SrShut.Cqrs.Domains;
 using SrShut.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ArlequimPetShop.Domain.Users
 {
     public class User : AggregateRoot<Guid>
     {
+        private string _password;
+
         public User()
         {
             Logins = new List<UserLogin>();
@@ -45,7 +50,17 @@ namespace ArlequimPetShop.Domain.Users
 
         [StringLength(maximumLength: 50, ErrorMessage = "A senha precisa ter entre {2} e {1} characteres.", MinimumLength = 6)]
         [RequiredValidator(ErrorMessage = "Senha do usuário obrigatório")]
-        public virtual string Password { get; set; }
+        public string Password
+        {
+            get => _password;
+            private set
+            {
+                if (!IsStrongPassword(value, out var error))
+                    throw new BusinessException(error);
+
+                _password = value;
+            }
+        }
 
         [RequiredValidator(ErrorMessage = "Data de criação do usuário obrigatório")]
         public virtual DateTime CreatedOn { get; set; }
@@ -56,6 +71,8 @@ namespace ArlequimPetShop.Domain.Users
         public virtual DateTime? DeletedOn { get; set; }
 
         public virtual IList<UserLogin> Logins { get; set; }
+
+        public void SetPassword(string password) => Password = password;
 
         public string Login(string email, string secret)
         {
@@ -88,6 +105,43 @@ namespace ArlequimPetShop.Domain.Users
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool IsStrongPassword(string password, out List<ErrorMessage> error)
+        {
+            error = new List<ErrorMessage>();
+
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
+            {
+                error.Add(new ErrorMessage("Password.Error", "A senha deve ter pelo menos 6 caracteres."));
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[A-Z]"))
+            {
+                error.Add(new ErrorMessage("Password.Error", "A senha deve conter pelo menos uma letra maiúscula."));
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[a-z]"))
+            {
+                error.Add(new ErrorMessage("Password.Error", "A senha deve conter pelo menos uma letra minúscula."));
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[0-9]"))
+            {
+                error.Add(new ErrorMessage("Password.Error", "A senha deve conter pelo menos um número."));
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[^a-zA-Z0-9]"))
+            {
+                error.Add(new ErrorMessage("Password.Error", "A senha deve conter pelo menos um caractere especial."));
+                return false;
+            }
+
+            return true;
         }
 
         public void Update(string name, string email, string password)
